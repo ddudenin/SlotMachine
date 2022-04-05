5174f8a5e30217e7ec9e8ffd286ff574424c978b
Dmitry <dmitry_dudenin@mail.ru>
2022-04-06 00:02:19 +0300

    initial commit

---
+++ SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Дмитрий Дуденин on 06.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
